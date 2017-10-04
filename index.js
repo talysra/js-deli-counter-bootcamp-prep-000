eff860fcf45fe8b1c3da6ec1b3551b23f4bf6927
Stanislava Koklina <medula_oblomgata@mail.ru>
2017-10-04 13:30:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var katzDeli = [];
 
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
-  return `Welcome ${name}. You are number ${katzDeliLine.length} in line.`
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
